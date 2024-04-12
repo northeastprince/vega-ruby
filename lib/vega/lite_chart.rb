18d13122adc45ae8f444988768305b121109b2d1
Matt Almeida <matt7@hey.com>
2024-04-12 11:06:56 -0400

    Allow specifying columns

--- lib/vega/lite_chart.rb
+++ lib/vega/lite_chart.rb
@@ -2,10 +2,12 @@ module Vega
   class LiteChart < BaseChart
     # https://vega.github.io/vega-lite/docs/spec.html
     scalar_methods \
-      :background, :padding, :autosize, :title, :name, :description, :width, :height, :mark, :spec, :repeat
+      :background, :padding, :autosize, :title, :name, :description,
+      :width, :height, :mark, :spec, :repeat, :columns
 
     hash_methods \
-      :config, :usermeta, :projection, :datasets, :encoding, :facet, :resolve, :selection, :view
+      :config, :usermeta, :projection, :datasets, :encoding,
+      :facet, :resolve, :selection, :view
 
     array_methods \
       :transform, :layer, :hconcat, :vconcat, :concat, :params
